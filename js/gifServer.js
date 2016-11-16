42443cf8f87bab5c3c0c0fe7341a839215e20812
nadyac <nadyacpena@gmail.com>
2016-11-15 21:20:06 -0500

    omg fixed so much stuff

--- js/gifServer.js
+++ js/gifServer.js
@@ -2,17 +2,31 @@ fs = require('fs');
 http = require('http');
 url = require('url');
 
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
 
 http.createServer(function(req, res){
   var request = url.parse(req.url, true);
   var action = request.pathname;
 
+
   if (action !== null && action !== "" && action !== "/") {
      console.log(action);
      try{
-        var img = fs.readFileSync('../' +action)
-        res.writeHead(200, {'Content-Type': 'image/gif' });
-        res.end(img, 'binary');
+        var base64str = base64_encode('../' +action);
+        //var img = fs.readFileSync('../' +base64str);
+        // Set CORS headers
+        res.writeHead(200, {
+          'Content-Type': 'application/json', 
+          'Access-Control-Allow-Origin': '*'
+        });
+        //console.log(base64str);
+        res.end(base64str, 'String');
      } catch (err){
         res.writeHead(404, {'Content-Type': 'text/html' });
         res.end('Gif not found');
