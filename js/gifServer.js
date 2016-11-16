5d66a99f5e32bc20726d97d823fe3f58ffed3756
nadyac <nadyacpena@gmail.com>
2016-11-15 21:27:29 -0500

    better comments are better

--- js/gifServer.js
+++ js/gifServer.js
@@ -2,11 +2,11 @@ fs = require('fs');
 http = require('http');
 url = require('url');
 
-// function to encode file data to base64 encoded string
+/* function to encode file data to base64 encoded string */
 function base64_encode(file) {
-    // read binary data
+    /* read binary data */
     var bitmap = fs.readFileSync(file);
-    // convert binary data to base64 encoded string
+    /* convert binary data to base64 encoded string */
     return new Buffer(bitmap).toString('base64');
 }
 
@@ -14,20 +14,21 @@ http.createServer(function(req, res){
   var request = url.parse(req.url, true);
   var action = request.pathname;
 
-
+    /* Check that the requested gif path is valid
+    *  if valid then resturn the gif as base64 string
+    *  else return an error message
+    */
   if (action !== null && action !== "" && action !== "/") {
      console.log(action);
      try{
         var base64str = base64_encode('../' +action);
-        //var img = fs.readFileSync('../' +base64str);
-        // Set CORS headers
+        /* Set headers */
         res.writeHead(200, {
           'Content-Type': 'application/json', 
           'Access-Control-Allow-Origin': '*'
         });
-        //console.log(base64str);
         res.end(base64str, 'String');
-     } catch (err){
+     } catch (err){ /* catch errors */
         res.writeHead(404, {'Content-Type': 'text/html' });
         res.end('Gif not found');
      }
